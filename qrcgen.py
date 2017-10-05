ac5377bf6767ae0c797c7c9a9af754d59b1dc01c
yqsy <yqsy021@126.com>
2017-10-05 18:35:15 +0800

    update

--- qrcgen.py
+++ qrcgen.py
@@ -9,53 +9,61 @@ import os
 import argparse
 import sys
 
-PARSER = None
-ARGS = None
 PREFIX = None
 RESNAME = None
-def scan(direc): 
+
+
+def scan(direc):
     """
     Scan tree starting from direc
     """
     resources = []
     for path, dirs, files in os.walk(direc):
-        resources += [os.path.join(path,f) for f in files]
+        resources += [os.path.join(path, f) for f in files]
     write_to_qrc(resources)
-        
+
+
 def write_to_qrc(resources):
     """
     Write to the qrc file under the prefix specified
     """
-    with open('%s.qrc'%RESNAME.strip('/'),'w') as f:
-        f.write('<RCC>\n  <qresource prefix="%s">\n'%PREFIX)
+    with open('%s.qrc' % RESNAME.strip('/'), 'w') as f:
+        f.write('<RCC>\n  <qresource prefix="%s">\n' % PREFIX)
         for r in resources:
-            f.write('    <file>%s</file>\n'%r)
+            r = r.replace('\\', '/')
+            f.write('    <file>%s</file>\n' % r)
         f.write('  </qresource>\n</RCC>\n')
-        
+
+
 def valid_path(string):
     """
     check if the path entered is a valid one
     """
     if not os.path.isdir(string):
-        msg = "%s is not a valid directory"%string
+        msg = "%s is not a valid directory" % string
         raise argparse.ArgumentTypeError(msg)
     return string
 
+
 def main():
-    PARSER = argparse.ArgumentParser(description='Generates a qrc (Qt resource file) from all files on a directory tree.',
-    epilog='A directory.qrc file will be generated in the current directory')
-    PARSER.add_argument('directory',metavar='directory', 
-        type=valid_path,
-        help='A valid path, full or local.')
-    PARSER.add_argument('prefix',metavar='prefix',
-        type=str,
-        help='The prefix in the qrc file under which the resources will be available.')
+    global PREFIX
+    global RESNAME
+    parse = argparse.ArgumentParser(
+        description='Generates a qrc (Qt resource file) from all files on a directory tree.',
+        epilog='A directory.qrc file will be generated in the current directory')
+    parse.add_argument('directory', metavar='directory',
+                       type=valid_path,
+                       help='A valid path, full or local.')
+    parse.add_argument('prefix', metavar='prefix',
+                       type=str,
+                       help='The prefix in the qrc file under which the resources will be available.')
 
-    #~ parser.print_help()
-    ARGS = PARSER.parse_args()
-    PREFIX = ARGS.prefix
-    RESNAME = os.path.split(ARGS.directory)[-1]
-    scan(ARGS.directory)
+    # ~ parser.print_help()
+    args = parse.parse_args()
+    PREFIX = args.prefix
+    RESNAME = os.path.split(args.directory)[-1]
+    scan(args.directory)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
